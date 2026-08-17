004123cbe5bbd4c74047234607291f6b605a1775
Abdelrahime7 <abdououni67@gmail.com>
2026-08-17 10:10:44 +0100

    feat(firebase): update authentication datasource

--- lib/infrastructure/firebase/auth/  irebase_auth_data_source.dart
+++ lib/infrastructure/firebase/auth/  irebase_auth_data_source.dart
@@ -1,10 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:pet_care/core/services/authetication/auth_service.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:pet_care/features/authentication/data%20/user_data.dart';
 
 class FirebaseAuthDataSource {
   final FirebaseAuth _auth;
+    final GoogleSignIn _googleSignIn;
 
-  FirebaseAuthDataSource(this._auth);
+
+  FirebaseAuthDataSource(this._auth, {required GoogleSignIn googleSignIn}) : _googleSignIn = googleSignIn;
 
   Future<UserCredential> register(
     UserRequest request
@@ -15,6 +18,8 @@ class FirebaseAuthDataSource {
     );
   }
 
+  
+
   Future<UserCredential> login(
     UserRequest request
   ) {
@@ -23,4 +28,39 @@ class FirebaseAuthDataSource {
       password: request.password,
     );
   }
+
+
+  Future logout() =>  _auth.signOut();
+  
+
+
+   UserResponse?  getCurrentUser() {
+    final firebaseUser = _auth.currentUser;
+
+    if (firebaseUser == null) {
+      return null;
+    }
+
+    return (
+    uid: firebaseUser.uid,
+    email: firebaseUser.email!
+    );
+  }
+
+  Future<UserCredential> loginWithGoogle() async {
+    final googleUser = await _googleSignIn.authenticate();
+
+    final googleAuth = googleUser.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      idToken: googleAuth.idToken,
+    );
+
+    return _auth.signInWithCredential(credential);
+  }
+
+
+
+
+
 }
