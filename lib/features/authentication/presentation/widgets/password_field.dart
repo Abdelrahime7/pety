981cfaeff85a8945c2d4769b098d7460c46b72aa
Abdelrahime7 <abdououni67@gmail.com>
2026-08-18 19:15:49 +0100

    feat(auth): add login screen

---
+++ lib/features/authentication/presentation/widgets/password_field.dart
@@ -0,0 +1,67 @@
+
+
+  import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pet_care/core/constant/theme/app_colors.dart';
+import 'package:pet_care/core/constant/theme/app_style.dart';
+import 'package:pet_care/core/constant/widgets/custom_text_field.dart';
+
+Widget buildPasswordField(TextEditingController passwordController,
+ void Function() onSuffixIconPressed,bool obscurePassword )
+
+ {
+   
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Password',
+              style: AppStyle.semibold14
+            ),
+
+            GestureDetector(
+              onTap: _handleForgotPassword,
+              child: Text(
+                'Forgot?',
+                style:AppStyle.reguler14TextButton
+              ),
+            ),
+          ],
+        ),
+
+        SizedBox(height: 6.h),//
+
+        SizedBox(
+          height: 48.h,
+       child: CustomeTextField(
+  controller: passwordController,
+  isPassword: obscurePassword,
+  hintText: '••••••••••••',
+  prefixIcon: Icon(
+    Icons.lock_outline,
+    size: 18.sp,
+    color: AppColors.icon,
+  ),
+  suffixIcon: Icon(
+    obscurePassword
+        ? Icons.visibility_outlined
+        : Icons.visibility_off_outlined,
+    size: 18.sp,
+    color: AppColors.secondaryText,
+  ),
+  onSuffixIconPressed: onSuffixIconPressed ,
+  
+),
+          ),
+      ]
+      
+        );
+      
+  
+  }
+ void _handleForgotPassword() {
+    // Navigate to forgot password.
+  }
---
+++ lib/features/authentication/presentation/widgets/register_footer.dart
@@ -0,0 +1,27 @@
+
+
+  import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pet_care/core/constant/theme/app_style.dart';
+
+Widget buildRegisterFooter() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "Don't have an account?",
+          style: AppStyle.reguler14
+        ),
+
+        SizedBox(width: 4.w),
+
+        GestureDetector(
+          onTap: (){},
+          child: Text(
+            'Sign Up',
+            style:  AppStyle.reguler14TextButton
+            ),
+          )
+      ],
+    );
+  }
