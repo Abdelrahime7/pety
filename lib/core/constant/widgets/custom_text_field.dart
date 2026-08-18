232f59c7b9d79437133a754c38e0789770724831
Abdelrahime7 <abdououni67@gmail.com>
2026-08-18 19:15:03 +0100

    feat(ui): add theme and reusable form widgets

--- lib/core/constant/widgets/custom_text_field.dart
+++ lib/core/constant/widgets/custom_text_field.dart
@@ -5,62 +5,127 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pet_care/core/constant/theme/app_colors.dart';
 import 'package:pet_care/core/constant/theme/app_style.dart';
 
+
 class CustomeTextField extends StatelessWidget {
   final String? hintText;
- final Widget ?sufixIcon ;
- final bool ?ispassword;
- final  double ? width ; 
- final double ? height ;
- final TextEditingController controller;
- final String? Function(String?)? validator;
+  final Widget? suffixIcon;
+  final bool isPassword;
+  final double? width;
+  final double? height;
+  final TextEditingController controller;
+  final String? Function(String?)? validator;
+  final TextInputType? keyboardType;
+  final Widget? prefixIcon;
+  final VoidCallback? onSuffixIconPressed;
 
-  const CustomeTextField  ({super.key, this.hintText, 
-  this.sufixIcon, this.ispassword, this.width,
-   this.height, required this.controller , this.validator
-});
+  const CustomeTextField({
+    super.key,
+    this.hintText,
+    this.suffixIcon,
+    this.isPassword = false,
+    this.width,
+    this.height,
+    required this.controller,
+    this.validator,
+    this.keyboardType,
+    this.prefixIcon,
+    this.onSuffixIconPressed,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return 
-    SizedBox(
-      width:width ??331.w,
-      height: height ??50.h,
-      
+    return SizedBox(
+      width: width ?? double.infinity,
+      height: height ?? 48.h,
+      child: TextFormField(
+        controller: controller,
+        keyboardType: keyboardType,
+        validator: validator,
+        obscureText: isPassword,
+        cursorColor: AppColors.primary,
+        autofocus: false,
 
-     child:  TextFormField(
-      controller: controller,
-      validator: validator,
+        style: AppStyle.reguler14,
 
-      obscureText: ispassword??false,
-      cursorColor: AppColors.primary,
-      autofocus: false,
-      
-      decoration: InputDecoration(
-      filled: true,
-        fillColor: Color(0xffF7F8F9),
-        suffixIcon: sufixIcon,
-        hintText: hintText ?? "",
-        hintStyle: AppStyle.subtitle,
-        contentPadding: EdgeInsets.symmetric(horizontal: 16.w,vertical: 12.h),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8.r),
-          borderSide: const BorderSide(color: Color(0xffE8ECF4),width: 1)
-        ,
-        
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: const Color(0xFFF7F8F9),
 
-        )  ,
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8.r),
-          borderSide:  BorderSide(color:AppColors.primary,width: 1)
-      
-    ),
-    errorBorder: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(8.r), 
-      borderSide:  BorderSide(color:Colors.red,width: 1)
-      
-    )
-    )
-     )
+          hintText: hintText ?? '',
+          hintStyle: AppStyle.reguler14,
+
+          prefixIcon: prefixIcon,
+
+          suffixIcon: suffixIcon != null
+              ? IconButton(
+                  onPressed: onSuffixIconPressed,
+                  padding: EdgeInsets.zero,
+                  constraints: BoxConstraints(
+                    minWidth: 48.w,
+                    minHeight: 48.h,
+                    maxWidth: 48.w,
+                    maxHeight: 48.h,
+                  ),
+                  icon: suffixIcon!,
+                )
+              : null,
+
+          prefixIconConstraints: BoxConstraints(
+            minWidth: 48.w,
+            minHeight: 48.h,
+            maxHeight: 48.h,
+          ),
+
+          suffixIconConstraints: BoxConstraints(
+            minWidth: 48.w,
+            minHeight: 48.h,
+            maxWidth: 48.w,
+            maxHeight: 48.h,
+          ),
+
+          contentPadding: EdgeInsets.symmetric(
+            horizontal: 16.w,
+            vertical: 0,
+          ),
+
+          border: _inputBorder(),
+          enabledBorder: _inputBorder(),
+          focusedBorder: _focusedBorder(),
+
+          errorBorder: _errorBorder(),
+          focusedErrorBorder: _errorBorder(),
+        ),
+      ),
     );
   }
-}  
+
+  OutlineInputBorder _inputBorder() {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12.r),
+      borderSide: BorderSide(
+        color: AppColors.border,
+        width: 1.w,
+      ),
+    );
+  }
+
+  OutlineInputBorder _focusedBorder() {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12.r),
+      borderSide: BorderSide(
+        color: AppColors.primary,
+        width: 1.w,
+      ),
+    );
+  }
+
+  OutlineInputBorder _errorBorder() {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12.r),
+      borderSide: BorderSide(
+        color: Colors.red,
+        width: 1.w,
+      ),
+    );
+  }
+}
