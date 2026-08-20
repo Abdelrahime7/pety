506ff2d6c7986fcc7a307ce3b91ad45e8a928879
Abdelrahime7 <abdououni67@gmail.com>
2026-08-20 13:19:27 +0100

    fix(auth): fix navigation after authentication

--- lib/core/constant/routers/app_routers.dart
+++ lib/core/constant/routers/app_routers.dart
@@ -1,11 +1,14 @@
 import 'package:go_router/go_router.dart';
-//import 'package:pet_care/features/authentication/presentation/login_screen.dart';
+import 'package:pet_care/features/authentication/presentation/login_screen.dart';
 import 'package:pet_care/features/home_screen/home_page.dart';
 
 final String home = '/home';
 final String login = '/login';
 
 final appRouter = GoRouter(
-  initialLocation: home,
-  routes: [GoRoute(path: home, builder: (context, state) => const HomePage())],
+  initialLocation: login,
+  routes: [
+    GoRoute(path: login, builder: (context, state) => const LoginScreen()),
+    GoRoute(path:home,builder: (context, state)=> const HomePage())
+    ],
 );
