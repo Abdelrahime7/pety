1c99d1a3de176f506c36c248b32040a48add4d89
Abdelrahime7 <abdououni67@gmail.com>
2026-08-20 13:19:52 +0100

    chore(auth): update Firebase and Google Sign-In configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:pet_care/core/constant/routers/app_routers.dart';
 
 import 'package:firebase_core/firebase_core.dart';
@@ -18,6 +20,14 @@ void main()async {
  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  
+
+  await GoogleSignIn.instance.initialize(
+    serverClientId:
+        '721972208500-qgkmhmklka2k2ul6rdg38i8ru4cuqpit.apps.googleusercontent.com',
+  );
+
+
   await setupNotifications();
   runApp(
     ScreenUtilInit(
@@ -25,11 +35,13 @@ void main()async {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (_, child) {
-        return MaterialApp.router(
-
-          title: 'PetCare+',
-          routerConfig: appRouter,
-          theme: AppTheme.light,
+        return ProviderScope(
+          child: MaterialApp.router(
+          
+            title: 'PetPaw+',
+            routerConfig: appRouter,
+            theme: AppTheme.light,
+          ),
         );
       },
       
