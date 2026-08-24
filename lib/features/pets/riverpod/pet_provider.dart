7138982de4d601f149d2be3ffc74393a360f82b6
Abdelmoumene <yassinbenyahia17@gmail.com>
2026-08-24 16:41:22 -0400

    remove pet state

--- lib/features/pets/riverpod/pet_provider.dart
+++ lib/features/pets/riverpod/pet_provider.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pet_care/features/pets/riverpod/pet_notifier.dart';
-import 'package:pet_care/features/pets/riverpod/pet_state.dart';
 
 import 'package:pet_care/features/pets/domain/entity/pet.dart';
 
