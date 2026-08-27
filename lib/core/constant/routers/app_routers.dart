64988fcac4c1235fae36a211df47fa0a1e2a4b5f
Abdelrahime7 <abdououni67@gmail.com>
2026-08-27 10:31:31 +0100

    feat: add profile routes

--- lib/core/constant/routers/app_routers.dart
+++ lib/core/constant/routers/app_routers.dart
@@ -3,15 +3,10 @@ import 'package:go_router/go_router.dart';
 import 'package:pet_care/features/authentication/presentation/login_screen.dart';
 import 'package:pet_care/features/pets/screens/add_pet_screen.dart';
 import 'package:pet_care/features/pets/screens/pets_list_screen.dart';
-<<<<<<< HEAD
-import 'package:pet_care/features/users/presentation/upgrad_to_premium_screen.dart';
-import 'package:pet_care/features/users/presentation/profile_screen.dart';
-=======
-import 'package:pet_care/features/profile/profile_page.dart';
-import 'package:pet_care/features/profile/profile_info_screen.dart';
-import 'package:pet_care/features/upgradetopremium/upgrad_to_premium_screen.dart';
->>>>>>> main
 import 'package:pet_care/core/layout/main_layout.dart';
+import 'package:pet_care/features/users/presentation/personal_information.dart';
+import 'package:pet_care/features/users/presentation/profile_screen.dart';
+import 'package:pet_care/features/users/presentation/upgrad_to_premium_screen.dart';
 
 final String profile = '/profile';
 final String login = '/login';
@@ -25,16 +20,10 @@ final appRouter = GoRouter(
   routes: [
 
     /// Screens WITHOUT bottom nav
-<<<<<<< HEAD
-    GoRoute(path: login, builder: (_, _) => const LoginScreen()),
-    GoRoute(path: addNewPet, builder: (_, _) => const AddPetScreen()),
-    GoRoute(path: upgradeToPremium, builder: (_, _) => const UpgradeToPremiumScreen()),
-=======
     GoRoute(path: login, builder: (_, __) => const LoginScreen()),
     GoRoute(path: addNewPet, builder: (_, __) => const AddPetScreen()),
     GoRoute(path: upgradeToPremium, builder: (_, __) => const UpgradeToPremiumScreen()),
     GoRoute(path: profileInfo, builder: (_, __) =>  ProfileInfoScreen()),
->>>>>>> main
 
     /// Screens WITH bottom nav
     ShellRoute(
