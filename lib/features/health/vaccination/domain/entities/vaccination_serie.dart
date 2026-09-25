7aac696cd013f6409e9b2f1b7dee14069e2ae0af
Abdelrahime7 <abdououni67@gmail.com>
2026-09-25 17:41:46 +0100

    refactor: clean up unused files and optimize pet details

--- lib/features/health/vaccination/domain/entities/vaccination_serie.dart
+++ lib/features/health/vaccination/domain/entities/vaccination_serie.dart
@@ -11,9 +11,7 @@ class VaccinationSerie {
 
   /// Total number of doses required to complete the series.
   final int requiredDoses;
-  /// number of doses completed
   final int completedDoses ;
-  /// 
   final bool isCompleted;
 
   /// Optional description of the vaccination series.
